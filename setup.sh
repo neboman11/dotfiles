35c1f5b4d855df6d8c692040c53f443117380ba0
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2020-06-09 10:45:10 -0500

    Added toggle for setting up zsh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@
 
 # Parsing variables
 DOOM_EMACS=true
+ZSH_SETUP=true
 
 # Help Function
 show_help () {
@@ -15,10 +16,12 @@ show_help () {
     echo "  Print this help statement and exit."
     echo "-d"
     echo "  Do not setup doom emacs."
+    echo "-z"
+    echo "  Do not setup zsh."
 }
 
 # Parse command line options using getopts
-while getopts "h?d" opt; do
+while getopts "h?dz" opt; do
     case "$opt" in
     h|\?)
         show_help
@@ -26,34 +29,38 @@ while getopts "h?d" opt; do
         ;;
     d)  DOOM_EMACS=false
         ;;
+    z)  ZSH_SETUP=false
+        ;;
     esac
 done
 
-# Download .zshrc file from this repo
-echo "Downloading .zshrc..."
-curl -sS https://raw.githubusercontent.com/neboman11/dotfiles/master/.zshrc -o ~/.zshrc
+if [ ZSH_SETUP == true ]; then
+    # Download .zshrc file from this repo
+    echo "Downloading .zshrc..."
+    curl -sS https://raw.githubusercontent.com/neboman11/dotfiles/master/.zshrc -o ~/.zshrc
 
-# Create .zsh folder to store the zsh plugins
-echo "Creating zsh folder to install plugins into..."
-mkdir -p ~/.zsh
+    # Create .zsh folder to store the zsh plugins
+    echo "Creating zsh folder to install plugins into..."
+    mkdir -p ~/.zsh
 
-# Download the zsh plugins and place them in ~/.zsh
-echo "Downloading zsh plugins..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
+    # Download the zsh plugins and place them in ~/.zsh
+    echo "Downloading zsh plugins..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
+    git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 
-# Check if the current shell is zsh
-if [ $(echo $SHELL | awk 'BEGIN { zsh=0 } $1 ~ /zsh/ { zsh=1 } END { print zsh }') -eq 1 ]; then # The test checks if zsh is the current shell, there is probably a better way to do this
-    # Source the new .zshrc file
-    echo "Sourcing new .zshrc file..."
-    source ~/.zshrc
-else
-    # Change shell to zsh
-    echo "Changing shell to zsh..."
-    chsh -s $(awk '$1 ~ /zsh/ { print; exit }' /etc/shells) # This will find the first line of the /etc/shells file containing 'zsh'
-fi
+    # Check if the current shell is zsh
+    if [ $(echo $SHELL | awk 'BEGIN { zsh=0 } $1 ~ /zsh/ { zsh=1 } END { print zsh }') -eq 1 ]; then # The test checks if zsh is the current shell, there is probably a better way to do this
+        # Source the new .zshrc file
+        echo "Sourcing new .zshrc file..."
+        source ~/.zshrc
+    else
+        # Change shell to zsh
+        echo "Changing shell to zsh..."
+        chsh -s $(awk '$1 ~ /zsh/ { print; exit }' /etc/shells) # This will find the first line of the /etc/shells file containing 'zsh'
+    fi
+fi # zsh setup
 
 if [ DOOM_EMACS == true ]; then
     # Setup doom emacs
@@ -73,4 +80,4 @@ if [ DOOM_EMACS == true ]; then
     echo '(setq evil-default-state "emacs")' >> ~/.emacs.d/modules/editor/evil/config.el
 
     echo "Done."
-fi
+fi # doom emacs setup
