9df4f8997eb9bfacc72c9aa86a17d494a0fc70da
Michael Nesbitt <michael.nesbitt111@gmail.com>
2024-01-27 19:57:57 -0600

    Added ripgrep to desired package list

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ DOOM_EMACS=true
 ZSH_SETUP=true
 INSTALL_PACKAGES=false
 
-DESIRED_PACKAGES="git zsh emacs-nox fzf tmux byobu bpytop"
+DESIRED_PACKAGES="git zsh emacs-nox fzf tmux byobu bpytop ripgrep"
 
 # Help Function
 show_help () {
