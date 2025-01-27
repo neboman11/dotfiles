ec4d2ded892b360abda19571e895de13d21f658d
Michael Nesbitt <michael.nesbitt111@gmail.com>
2025-01-27 11:21:11 -0600

    Fixed doom emacs config location

--- setup.sh
+++ setup.sh
@@ -90,7 +90,7 @@ if [ "$DOOM_EMACS" = true ]; then
 
     # Set evil to default to emacs mode
     echo "Setting emacs as the default editor mode..."
-    echo '(setq evil-default-state "emacs")' >> ~/.doom.d/config.el
+    echo '(setq evil-default-state "emacs")' >> ~/.config/doom/config.el
 
     echo "Done."
 fi # doom emacs setup
